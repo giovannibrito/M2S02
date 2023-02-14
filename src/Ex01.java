a6123ec3a7976d5513e99cec4bfe99aa4f3704c2
giovannibrito <giovannileonibrito@bol.com.br>
2023-02-14 17:57:11 -0300

    Adiciona ex 1

---
+++ src/Ex01.java
@@ -0,0 +1,15 @@
+public class Ex01 {
+
+    public static void main(String[] args) {
+        int[] arr = { 56, 57, 58, 67, 99, 12, 11, 88 };
+
+        // Imprima o valor do primeiro elemento:
+        System.out.println(arr[0]); // 56
+
+        // Imprima o valor do ultimo elemento:
+        System.out.println(arr[arr.length-1]); //88
+
+        // Imprima o valor do terceiro elemento:
+        System.out.println(arr[2]); // 58
+    }
+}
